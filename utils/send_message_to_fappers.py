4b7a68714777723b3f65545fd51537bf52f5efe8
ZhorovIvan <jorov.and@yandex.ru>
2023-03-20 17:32:07 +0300

    change message text

--- utils/send_message_to_fappers.py
+++ utils/send_message_to_fappers.py
@@ -7,13 +7,13 @@ import datetime
 async def send_message():
     user_dates = config.USER_TIME_DATES
     text = '\n'.join([ get_text_message_for_fapper(user_name, user_dates[user_name]) for user_name in user_dates.keys() ])
-    text += '\nНу чё гребни ебаные блять, баба же не нужна?!'
+    text += '\nНе хмурь бровей из-за ударов рока,\nУпавший духом, гибнет раньше срока.'
     await bot.send_message(chat_id=config.CHAT_FAPPERS_ID, text=text)
 
 
 def get_text_message_for_fapper(user_name, user_dates):
     days_without_fap = (datetime.date.today() - datetime.date(year=int(user_dates["yyyy"]),month=int(user_dates["M"]),day=int(user_dates["dd"]))).days
-    return f"{user_name} не дрочил {days_without_fap} суток."
+    return f"{user_name} не дрочил и не смотрел порно {days_without_fap} суток."
 
 
 async def scheduler():
